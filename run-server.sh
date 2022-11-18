9409856aa64eabb0dc83191d33294b1368232f1a
Charles Cao <caotaotao@gmail.com>
2022-11-18 05:37:28 -0500

    update server

--- run-server.sh
+++ run-server.sh
@@ -2,7 +2,8 @@
 export LC_ALL="C.UTF-8"
 export LANG="C.UTF-8"
 export FLASK_APP=./server.py
-
+export DOMAIN="18.221.71.211"
+export SQLALCHEMY_DATABASE_URI="sqlite:///users.db"
 if [ ! -d venv ]
 then
     echo "[+] Creating new python3 virtualenv named venv"
@@ -15,7 +16,7 @@ pip3 install -r requirements.txt
 while [ 1 == 1 ]
 do
   #echo `date` >> start.log
-  flask run # --with-threads --host=0.0.0.0
+  flask run  --with-threads --host=0.0.0.0
   sleep 5
 done
 
