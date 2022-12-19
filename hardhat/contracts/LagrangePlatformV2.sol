968e5960991a4d2234ee35d96d9822cbf0fef2f0
rykci <yuen.ricky@hotmail.com>
2022-12-19 10:04:20 -0500

    platformV2: reward + claim design

--- hardhat/contracts/LagrangePlatformV2.sol
+++ hardhat/contracts/LagrangePlatformV2.sol
@@ -2,27 +2,28 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract LagrangePlatform is Initializable, OwnableUpgradeable {
+contract LagrangePlatformV2 is Ownable {
     IERC20 public lagrangeToken;
+
+    mapping(address => uint) claimBalance;
 
     event DataUpload(address uploader, uint size, uint reward);
     event ModelUpload(address uploader, uint reward);
+    event Claim(address claimer, uint amount);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(address lagrangeTokenAddress) initializer public {
+    constructor(address lagrangeTokenAddress) {
         lagrangeToken = IERC20(lagrangeTokenAddress);
-        __Ownable_init();
     }
 
+
+    /*
+    * owner rewards user
+    * user claims on their own
+    */
     function rewardDataUpload(address uploader, uint size) public onlyOwner {
         require(uploader != address(0), 'zero address cannot be an uploader');
         require(size > 0, 'data cannot be size 0');
@@ -36,18 +37,35 @@ contract LagrangePlatform is Initializable, OwnableUpgradeable {
             reward = 0.5 ether * numGB;
         }
 
-        require(lagrangeToken.balanceOf(address(this)) > reward, 'not enough contract balance to pay uploader');
+        claimBalance[uploader] += reward;
+        // require(lagrangeToken.balanceOf(address(this)) > reward, 'not enough contract balance to pay uploader');
 
-        lagrangeToken.transfer(uploader, reward);
+        // lagrangeToken.transfer(uploader, reward);
         emit DataUpload(uploader, size, reward);
     }
 
     function rewardModelUpload(address uploader) public onlyOwner {
         uint reward = 2 ether;
-        require(lagrangeToken.balanceOf(address(this)) > reward, 'not enough contract balance to pay uploader');
+        //require(lagrangeToken.balanceOf(address(this)) > reward, 'not enough contract balance to pay uploader');
 
-        lagrangeToken.transfer(uploader, reward);
+        //lagrangeToken.transfer(uploader, reward);
+        claimBalance[uploader] += reward;
         emit ModelUpload(uploader, reward);
+    }
+
+    function claim() public {
+        uint claimAmount = claimBalance[msg.sender];
+        require(claimAmount > 0, 'no balance to claim');
+
+        uint contractBalance = lagrangeToken.balanceOf(address(this));
+        if (contractBalance < claimAmount) {
+            claimAmount = contractBalance;
+        }
+
+        claimBalance[msg.sender] -= claimAmount;
+        lagrangeToken.transfer(msg.sender, claimAmount);
+
+        emit Claim(msg.sender, claimAmount);
     }
 
     function withdraw(uint amount) public onlyOwner {
