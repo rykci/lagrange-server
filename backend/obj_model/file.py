b25adf05f93632e8926ef6941f9316683934b2cb
Charles Cao <caotaotao@gmail.com>
2023-01-12 20:08:09 -0500

    create file upload test using MCS 2.O (#20)

--- backend/obj_model/file.py
+++ backend/obj_model/file.py
@@ -1,22 +1,26 @@
+import json
 import os
 
 from dotenv import load_dotenv
 
 
 class File:
-    def __init__(self, filename, file_path):
-        self.filename = filename
-        self.chain_name = "polygon.mainnet"
-        self.wallet_address = os.getenv('wallet_address')
-        self.private_key = os.getenv('private_key')
-        self.rpc_endpoint = os.getenv('rpc_endpoint')
+    def __init__(self, file_data):
+        self.name = file_data["Name"]
+        self.address = file_data["Address"]
+        self.bucket_uid = file_data["BucketUid"]
+        self.filehash = file_data["FileHash"]
+        self.prefix = file_data["Prefix"]
+        self.size = file_data["Size"]
+        self.payloadCid = file_data["PayloadCid"]
+        self.ipfs_url = file_data["IpfsUrl"]
+        self.pin_status = file_data["PinStatus"]
+        self.is_deleted = file_data["IsDeleted"]
+        self.is_folder = file_data["IsFolder"]
+        self.updated_at = file_data["UpdatedAt"]
+        self.created_at = file_data["CreatedAt"]
+        self.deleted_at = file_data["DeletedAt"]
 
-        self.file_path = file_path
-        self.upload_response = None
-        self.payment_tx_hash = None
-
-
-
-if __name__ == '__main__':
-    load_dotenv("../../.env")
-    file = File('n012.jpg', '../dataset/n009.jpg')
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
