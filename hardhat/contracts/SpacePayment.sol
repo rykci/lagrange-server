8f67f12e7df91380853b23009defbe8cdd01eecf
rykci <yuen.ricky@hotmail.com>
2023-01-13 01:51:09 -0500

    add duration to space payment

--- hardhat/contracts/SpacePayment.sol
+++ hardhat/contracts/SpacePayment.sol
@@ -2,49 +2,110 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "./LagrangeDAOToken.sol";
 
 contract SpacePayment is Ownable {
-    mapping(address => uint) private balance;
+    using Counters for Counters.Counter;
+    Counters.Counter private spaceCounter;
+
+    mapping(uint256 => Space) public idToSpace;
+    mapping(address => uint256) private balance;
 
     // rate is 1 LAD = $0.03
-    mapping(uint => uint) public hardwareToPricePerHour;
+    mapping(uint256 => uint256) public hardwareToPricePerEpoch;
+    uint256 public epochDuration = 1 hours;
+
+    struct Space {
+        address owner;
+        uint256 hardwareType;
+        uint256 expiryTime;
+    }
 
     LagrangeDAOToken public ladToken;
+
+    event SpaceCreated(uint256 id, address owner, uint256 hardwareType, uint256 expiryTime);
+    event ExpiryExtended(uint256 id, uint256 expiryTime);
+    event EpochDurationChanged(uint256 epochDuration);
+    event HardwarePriceChanged(uint256 hardwareType, uint256 price);
 
     constructor(address tokenAddress) {
         ladToken = LagrangeDAOToken(tokenAddress);
 
-        hardwareToPricePerHour[1] = 0;
-        hardwareToPricePerHour[2] = 1;
-        hardwareToPricePerHour[3] = 20;
-        hardwareToPricePerHour[4] = 30;
-        hardwareToPricePerHour[5] = 35;
-        hardwareToPricePerHour[6] = 105;
+        hardwareToPricePerEpoch[1] = 0 ether;
+        hardwareToPricePerEpoch[2] = 1 ether;
+        hardwareToPricePerEpoch[3] = 20 ether;
+        hardwareToPricePerEpoch[4] = 30 ether;
+        hardwareToPricePerEpoch[5] = 35 ether;
+        hardwareToPricePerEpoch[6] = 105 ether;
     }
 
-    function deposit(uint amount) public {
-        require(ladToken.allowance(msg.sender, address(this)) >= amount, "ERC20: allowance is too low");
+    function deposit(uint256 amount) public {
+        require(
+            ladToken.allowance(msg.sender, address(this)) >= amount,
+            "ERC20: allowance is too low"
+        );
 
         ladToken.transferFrom(msg.sender, address(this), amount);
         balance[msg.sender] += amount;
     }
 
-    function balanceOf(address account) public view returns (uint) {
+    function balanceOf(address account) public view returns (uint256) {
         return balance[account];
     }
 
-    function buySpace(uint hardwareType) public {
-        require(balance[msg.sender] >= hardwareToPricePerHour[hardwareType], "not enough balance");
+    function buySpace(uint256 hardwareType, uint256 duration) public {
+        uint256 price = (hardwareToPricePerEpoch[hardwareType] * duration) / epochDuration;
+        require(balance[msg.sender] >= price, "not enough balance");
 
-        balance[msg.sender] -= hardwareToPricePerHour[hardwareType];
+        uint256 spaceId = spaceCounter.current();
+        spaceCounter.increment();
+
+        uint256 expiryTime = block.timestamp + duration;
+        balance[msg.sender] -= price;
+        idToSpace[spaceId] = Space(msg.sender, hardwareType, expiryTime);
+
+        emit SpaceCreated(spaceId, msg.sender, hardwareType, expiryTime);
     }
 
-    function withdraw(uint amount) public onlyOwner {
+    function extendSpace(uint256 spaceId, uint256 duration) public {
+        Space memory space = idToSpace[spaceId];
+        require(space.expiryTime > 0, "space not found");
+        uint256 price = (hardwareToPricePerEpoch[space.hardwareType] * duration) / epochDuration;
+        require(balance[msg.sender] >= price, "not enough balance");
+
+        balance[msg.sender] -= price;
+        if (isExpired(spaceId)) {
+            idToSpace[spaceId].expiryTime += block.timestamp + duration;
+        } else {
+            idToSpace[spaceId].expiryTime += duration;
+        }
+
+        emit ExpiryExtended(spaceId, idToSpace[spaceId].expiryTime);
+    }
+
+    function isExpired(uint256 spaceId) public view returns (bool) {
+        return idToSpace[spaceId].expiryTime <= block.timestamp;
+    }
+
+    function spaceInfo(uint256 spaceId) public view returns (Space memory) {
+        return idToSpace[spaceId];
+    }
+
+    function changeEpochDuration(uint256 newDuration) public onlyOwner {
+        epochDuration = newDuration;
+        emit EpochDurationChanged(newDuration);
+    }
+
+    function changeHardwarePrice(uint256 hardwareType, uint256 newPrice) public onlyOwner {
+        hardwareToPricePerEpoch[hardwareType] = newPrice;
+        emit HardwarePriceChanged(hardwareType, newPrice);
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
         require(amount <= ladToken.balanceOf(address(this)), "not enough tokens to withdraw");
 
         ladToken.transfer(msg.sender, amount);
     }
-
 }
